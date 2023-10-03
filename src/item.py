9cf8364025d48191492ce51862e8fee858610c24
Дмитрий <dfeskov1998@mail.ru>
2023-10-03 22:10:38 +0300

    add class,static and property methods

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +15,24 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+        if len(self.__name) <= 10:
+            return self.__name
+        else:
+            return self.__name[:11]
+
 
     def calculate_total_price(self) -> float:
         """
@@ -26,8 +42,32 @@ class Item:
         """
         return self.price * self.quantity
 
+
     def apply_discount(self):
         """
         Применяет установленную скидку для конкретного товара.
         """
         return self.price * Item.pay_rate
+
+
+    @classmethod
+    def instantiate_from_csv(cls, filename):
+        """Класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv"""
+
+        with open(filename, encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            items = []
+            for row in reader:
+                name = str(row["name"])
+                price = int(row["price"])
+                quantity = int(row["quantity"])
+                item = cls(name, price, quantity)
+                items.append(item)
+            cls.all = items
+
+
+    @staticmethod
+    def string_to_number(string: str) -> None:
+        """Статический метод, возвращающий число из числа-строки"""
+
+        return int(float(string))
