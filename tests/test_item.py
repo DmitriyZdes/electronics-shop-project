5c77804a772e39bea1ad93d16a0db3644ad2a1c5
Дмитрий <dfeskov1998@mail.ru>
2023-10-03 22:11:01 +0300

    add new tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,10 +2,23 @@
 
 from src.item import Item
 
+
 def test_calculate_total_price():
     item_1 = Item("Cмартфон", 45000.8, 10)
     assert item_1.calculate_total_price() == 450008.0
 
+
 def test_apply_discount():
-    item_2 = Item("Телефизор", 20000.5, 50)
+    item_2 = Item("Телевизор", 20000.5, 50)
     assert item_2.apply_discount() == 20000.5
+
+
+def test_string_to_number():
+    string = "11"
+    number = 11
+    assert Item.string_to_number(string) == number
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')  # создание объектов из данных файла
+    assert len(Item.all) == 5
