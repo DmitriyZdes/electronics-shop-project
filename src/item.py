8177ea0bdde9494a40ca3d160b9c3b6738af02fa
Дмитрий <dfeskov1998@mail.ru>
2023-10-07 00:00:19 +0300

    add repr and str methods

--- src/item.py
+++ src/item.py
@@ -33,6 +33,11 @@ class Item:
         else:
             return self.__name[:11]
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self.name
 
     def calculate_total_price(self) -> float:
         """
